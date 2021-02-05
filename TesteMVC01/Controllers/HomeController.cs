9ebf77fecc6430a0185e328937b9c9a777a390e9
009Schmitt <34982675+009Schmitt@users.noreply.github.com>
2021-02-05 20:41:25 -0300

    Agora esta Correto
    Tem bastante conteúdo nesse arquivo

---
+++ TesteMVC01/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TesteMVC01.Models;
+
+namespace TesteMVC01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var pessoa = new Pessoa
+            {
+                idPessoa = 1,
+                Nome = "Joeslei",
+                Tipo = "Heroi"
+            };
+
+            //ViewData["idPessoa"] = pessoa.idPessoa;
+            //ViewData["Nome"] = pessoa.Nome;
+            //ViewData["Tipo"] = pessoa.Tipo;
+
+            //ViewBag.id = pessoa.idPessoa;
+            //ViewBag.nome = pessoa.Nome;
+            //ViewBag.tipo = pessoa.Tipo;
+
+            //return View();
+
+            return View(pessoa);
+        }
+
+        [HttpPost] // esse método so pode ser usado em requisições HTTP
+        public IActionResult Lista(int pessoaId, string nome, string tipo)
+        {
+            ViewData["idPessoa"] = pessoaId;
+            ViewData["Nome"] = nome;
+            ViewData["Tipo"] = tipo;
+
+            return View();
+        }
+
+        public IActionResult Contatos()
+        {
+            return View();
+        }
+    }
+}
---
+++ TesteMVC01/Models/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteMVC01.Models
+{
+    public class Pessoa
+    {
+        public int idPessoa { get; set; }
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+    }
+}
